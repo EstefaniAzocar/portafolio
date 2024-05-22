9a56398452a044053b73e2a7d0b17ddcd6efdff9
estefaniazocar <estefanyazocar3@gmail.com>
2024-05-21 22:50:15 -0500

    terminado la data de proyectos

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./carousel.css"; // Importa tu archivo CSS
 
-export function Carousel ({children}) {
+export function Carousel({ children }) {
   const gap = 16;
   const carouselRef = useRef(null);
   const contentRef = useRef(null);
@@ -31,32 +31,48 @@ export function Carousel ({children}) {
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
-  }, ); // La dependencia vacía [] asegura que este efecto se ejecute solo una vez
+  }, []); // La dependencia vacía [] asegura que este efecto se ejecute solo una vez
 
   const handleNextClick = () => {
     carouselRef.current.scrollBy({ left: width + gap, behavior: 'smooth' });
-    if (carouselRef.current.scrollWidth !== 5) {
-      prevRef.current.style.display = 'flex';
-    }
-    if (contentRef.current.scrollWidth - width - gap <= carouselRef.current.scrollLeft + width) {
+
+    // Mostrar botón "Prev"
+    prevRef.current.style.display = 'flex';
+
+    // Verificar si estamos al final del contenido
+    if (carouselRef.current.scrollLeft + width + gap >= contentRef.current.scrollWidth) {
       nextRef.current.style.display = 'none';
     }
   };
 
   const handlePrevClick = () => {
     carouselRef.current.scrollBy({ left: -(width + gap), behavior: 'smooth' });
-    if (carouselRef.current.scrollLeft - width - gap <= 5) {
+
+    // Mostrar botón "Next"
+    nextRef.current.style.display = 'flex';
+
+    // Verificar si estamos al principio del contenido
+    if (carouselRef.current.scrollLeft - width - gap <= 0) {
       prevRef.current.style.display = 'none';
-    }
-    if (!(contentRef.current.scrollWidth - width - gap <= carouselRef.current.scrollLeft + width)) {
-      nextRef.current.style.display = 'flex';
     }
   };
 
+  useEffect(() => {
+    // Inicialmente, ocultar el botón "Prev" porque estamos al principio
+    prevRef.current.style.display = 'none';
+
+    // Verificar si necesitamos mostrar o ocultar el botón "Next" inicialmente
+    if (contentRef.current.scrollWidth <= carouselRef.current.offsetWidth) {
+      nextRef.current.style.display = 'none';
+    } else {
+      nextRef.current.style.display = 'flex';
+    }
+  }, [width]);
+
   return (
     <div className="carouselWrapper">
-      <div id="wrapper" >
-        <div id="carousel" ref={carouselRef} >
+      <div id="wrapper">
+        <div id="carousel" ref={carouselRef}>
           <div id="content" ref={contentRef}>
             {children}
           </div>
@@ -72,7 +88,7 @@ export function Carousel ({children}) {
             <path d="M15.61 7.41L14.2 6l-6 6 6 6 1.41-1.41L11.03 12l4.58-4.59z" />
           </svg>
         </button>
-        <button className="next"  onClick={handleNextClick} ref={nextRef}>
+        <button className="next" onClick={handleNextClick} ref={nextRef}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -83,8 +99,9 @@ export function Carousel ({children}) {
             <path d="M10.02 6L8.61 7.41 13.19 12l-4.58 4.59L10.02 18l6-6-6-6z" />
           </svg>
         </button>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
+
 export default Carousel;
